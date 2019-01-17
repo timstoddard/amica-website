bd231919025b807c6cc7a83a0dbef4ce4594fba1
Tim Stoddard <tim.stoddard2@gmail.com>
2019-01-17 13:55:04 -0800

    add simple homepage

--- src/amica/Amica.tsx
+++ src/amica/Amica.tsx
@@ -6,6 +6,16 @@ const Amica: React.StatelessComponent<{}> = (): JSX.Element => {
   document.title = 'Amica'
   return (
     <div className='homepage'>
+      <header className='homepage__header'>
+        <div className='homepage__header__title'>Amica</div>
+        <div className='homepage__header__links'>
+          <div className='homepage__header__link'>Games</div>
+          <div className='homepage__header__link'>About</div>
+          <div className='homepage__header__link'>Pricing</div>
+          <div className='homepage__header__link'>Sign up</div>
+          <div className='homepage__header__link'>Log in</div>
+        </div>
+      </header>
       <h1 className='homepage__title'>
         Welcome to Amica's homepage!
       </h1>
