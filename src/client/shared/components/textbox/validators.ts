56b2160196b47cdc0c8148cba431e69152343506
Tim Stoddard <tim.stoddard2@gmail.com>
2019-02-19 23:38:55 -0800

    add todo

--- src/client/shared/components/textbox/validators.ts
+++ src/client/shared/components/textbox/validators.ts
@@ -1,6 +1,7 @@
 import { AbstractControl, FormArray, FormGroup, ValidatorFn } from 'react-reactive-form'
 
 // assumes password fields are siblings in the same form
+// TODO the control this is on doesn't update when the sibling is changed (even if status should change)
 export const stringMatch = (otherFieldName: string): ValidatorFn =>
   (c: AbstractControl | FormGroup | FormArray) => {
     return c.parent && c.value !== c.parent.get(otherFieldName).value
